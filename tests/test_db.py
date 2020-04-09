f996d53ec3eb3f80f33fdd81fd1f03a9005efa9d
Yaroslav <maplegend@mail.ru>
2020-04-09 21:32:53 +0500

    Update tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,3 +1,7 @@
+import psycopg2
+import testing.postgresql
+
+
 from db import DB
 import datetime
 import constants
@@ -5,6 +9,9 @@ import constants
 
 def test_db():
     db = DB()
-    db.connect(constants.DB_HOST, constants.DB_USER, constants.DB_PASSWORD, constants.DB_NAME)
-    db.cur.execute("INSERT INTO django_session (session_key, session_data, expire_date) VALUES (%s, %s, %s)", ('123', "321", datetime.datetime.now()))
-    assert db.get_session_data('123') == '321'
+    with testing.postgresql.Postgresql() as postgresql:
+        db.conn = psycopg2.connect(**postgresql.dsn())
+        db.cur = db.conn.cursor()
+        db.cur.execute("CREATE TABLE django_session (session_key varchar(255),session_data varchar(255),expire_date varchar(255))")
+        db.cur.execute("INSERT INTO django_session (session_key, session_data, expire_date) VALUES (%s, %s, %s)", ('123', "321", datetime.datetime.now()))
+        assert db.get_session_data('123') == '321'
