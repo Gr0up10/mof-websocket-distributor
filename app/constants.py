a7e760c7ab973e79d418307aba9f8d5a92fac2f7
Yaroslav <maplegend@mail.ru>
2020-04-10 15:09:28 +0500

    Update constants

--- app/constants.py
+++ app/constants.py
@@ -1,14 +1,14 @@
 import os
 
-DB_NAME = os.getenv('DB_NAME', 'not found')
-DB_HOST = os.getenv('DB_HOST', 'not found')
-DB_USER = os.getenv('DB_USER', 'not found')
-DB_PASSWORD = os.getenv('DB_PASSWORD', 'not found')
+DB_NAME = os.getenv('DB_NAME', 'database')
+DB_HOST = os.getenv('DB_HOST', 'localhost')
+DB_USER = os.getenv('DB_USER', 'username')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'password')
 
-DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'not found')
+DJANGO_SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'ecw==078()bm0#u^f6))--6jz3nk27rwy04wb6=2f_3rqrsvq*')
 
-HANDLER_HOST = os.getenv('HANDLER_HOST', 'not found')
-HANDLER_PORT = os.getenv('HANDLER_PORT', 'not found')
+HANDLER_HOST = os.getenv('HANDLER_HOST', 'localhost')
+HANDLER_PORT = os.getenv('HANDLER_PORT', '58008')
 
-WS_HOST = os.getenv('WS_HOST', 'not found')
-WS_PORT = os.getenv('WS_PORT', 'not found')
+WS_HOST = os.getenv('WS_HOST', 'localhost')
+WS_PORT = os.getenv('WS_PORT', '4321')
